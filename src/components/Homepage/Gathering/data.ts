258a85e3150a8b77d2ddb92e073434f8c8e8e4e2
juvenile <juvenile@zklink.org>
2024-04-29 13:52:19 +0800

    feat: add gathering photo

--- src/components/Homepage/Gathering/data.ts
+++ src/components/Homepage/Gathering/data.ts
@@ -1,54 +1,18 @@
-import Icon1 from "../../../assets/gathering/1.jpeg";
-import Icon2 from "../../../assets/gathering/2.jpeg";
-import Icon3 from "../../../assets/gathering/3.jpeg";
-import Icon4 from "../../../assets/gathering/4.jpeg";
-import Icon5 from "../../../assets/gathering/5.jpeg";
-import Icon6 from "../../../assets/gathering/6.jpeg";
-import Icon7 from "../../../assets/gathering/7.jpeg";
-import Icon8 from "../../../assets/gathering/8.jpeg";
-import Icon9 from "../../../assets/gathering/9.jpeg";
-import Icon10 from "../../../assets/gathering/10.jpeg";
-import Icon11 from "../../../assets/gathering/11.jpeg";
-import Icon12 from "../../../assets/gathering/12.jpeg";
-import Icon13 from "../../../assets/gathering/13.jpeg";
-import Icon14 from "../../../assets/gathering/14.jpeg";
-import Icon15 from "../../../assets/gathering/15.jpeg";
-import Icon16 from "../../../assets/gathering/16.jpeg";
-import Icon17 from "../../../assets/gathering/17.jpeg";
-import Icon18 from "../../../assets/gathering/18.jpeg";
-import Icon19 from "../../../assets/gathering/19.jpeg";
-import Icon20 from "../../../assets/gathering/20.jpeg";
-import Icon21 from "../../../assets/gathering/21.jpeg";
-import Icon22 from "../../../assets/gathering/22.jpeg";
-import Icon23 from "../../../assets/gathering/23.jpeg";
-import Icon24 from "../../../assets/gathering/24.jpeg";
-import Icon25 from "../../../assets/gathering/25.jpeg";
 import { StaticImageData } from "next/image";
 
-export const imgs: StaticImageData[] = [
-  Icon1,
-  Icon2,
-  Icon3,
-  Icon4,
-  Icon5,
-  Icon6,
-  Icon7,
-  Icon8,
-  Icon9,
-  Icon10,
-  Icon11,
-  Icon12,
-  Icon13,
-  Icon14,
-  Icon15,
-  Icon16,
-  Icon17,
-  Icon18,
-  Icon19,
-  Icon20,
-  Icon21,
-  Icon22,
-  Icon23,
-  Icon24,
-  Icon25,
-];
+function imagesContext(): any {
+  return (require as any).context(
+    "../../../assets/gathering",
+    true, // Whether to recursively search subdirectories
+    /\.(jpg|jpeg|png|gif|svg)$/
+  );
+}
+
+const imagesContextKeys = imagesContext();
+
+const images = imagesContextKeys
+  .keys()
+  ?.filter((key: string) => key.includes("gathering"))
+  .map((key: string) => imagesContextKeys(key));
+
+export const imgs: StaticImageData[] = [...images];
